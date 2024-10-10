11f98d7717175d41ec3b726e2cfb3846fedd99e6
SergioRojasLopez <115539369+SergioRojasLopez@users.noreply.github.com>
2024-10-10 14:41:50 +0200

    procesos tema 1

---
+++ src/main/java/org/example/LanzaProceso.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Arrays;
+import java.io.IOException;
+
+public class LanzaProceso {
+
+  public static void main(String[] args) {
+
+    if (args.length <= 0) {
+      System.out.println("Debe indicarse comando a ejecutar.");
+      System.exit(1);
+    }
+
+    ProcessBuilder pb = new ProcessBuilder(args);
+    pb.inheritIO();
+
+    try {
+      Process p = pb.start();
+      int codRet = p.waitFor();
+      System.out.println("La ejecución de " + Arrays.toString(args)
+              + " devuelve " + codRet
+              + " " + (codRet == 0 ? "(ejecución correcta)" : "(ERROR)")
+      );
+    } catch (IOException e) {
+      System.err.println("Error durante ejecución del proceso");
+      System.err.println("Información detallada");
+      System.err.println("---------------------");
+      e.printStackTrace();
+      System.err.println("---------------------");
+      System.exit(2);
+    } catch (InterruptedException e) {
+      System.err.println("Proceso interrumpido");
+      System.exit(3);
+    }
+
+  }
+
+}
